26796c8d5c09a37d6bff0bbd9b2ed45a776eab34
suellenvenancio <suellen.venancio@hotmail.com>
2026-09-20 01:34:15 -0300

    alterar url de prod da api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://saber-flow-back-end.vercel.app',
+  apiUrl:
+    'https://saber-flow-api-c7gbg8edg7fscyh5.westus3-01.azurewebsites.net',
 };
